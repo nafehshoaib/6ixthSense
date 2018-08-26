e41b9b1c69e65db10be392926ecc9b958a2a116d
Nafeh Shoaib <nafehshoaib14@gmail.com>
2018-08-26 07:44:03 -0400

    Initial Commit

---
+++ SixthSense/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SixthSense
+//
+//  Created by Nafeh Shoaib on 2018-08-26.
+//  Copyright © 2018 Nafeh Shoaib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
